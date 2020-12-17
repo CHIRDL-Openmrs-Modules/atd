8beaaf92ad14156d38cecddae6f490c845af5c02
msheley <msheley@iu.edu>
2020-12-17 16:48:35 -0500

    Add decryption of global property values

--- omod/src/main/java/org/openmrs/module/atd/web/ImportConceptsFromFileServlet.java
+++ omod/src/main/java/org/openmrs/module/atd/web/ImportConceptsFromFileServlet.java
@@ -102,7 +102,7 @@ public class ImportConceptsFromFileServlet extends HttpServlet
 							// This also allows us to make sure an error didn't occur while parsing the file
 							List<ConceptDescriptor> list = ImportConceptsUtil.getConcepts(file.getInputStream());
 							
-							if(list != null && !list.isEmpty())
+							if(list != null && list.size() > 0)
 							{
 								importConcepts = new ImportConceptsUtil(file.getInputStream());
 								Thread thread = new Thread(importConcepts);
