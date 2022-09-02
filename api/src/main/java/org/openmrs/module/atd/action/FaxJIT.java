f296f3433c9989ecf5aea5efc4720982e87fd16d
sabhanda <sabhanda@140.182.66.200>
2022-09-02 12:39:25 -0400

    CHICA-1654 Velocity Vulnerability Fix

--- api/src/main/java/org/openmrs/module/atd/action/FaxJIT.java
+++ api/src/main/java/org/openmrs/module/atd/action/FaxJIT.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.openmrs.Form;
