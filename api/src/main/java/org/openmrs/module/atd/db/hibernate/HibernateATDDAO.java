7486260b4a0601c2f7dbb20af9dee677626f1c04
sjmckee <sjmckee@iupui.edu>
2022-09-16 10:43:36 -0400

    CHICA-1654 Velocity Vulnerability Fix (#114)
    Co-authored-by: sabhanda <sabhanda@140.182.66.200>

--- api/src/main/java/org/openmrs/module/atd/db/hibernate/HibernateATDDAO.java
+++ api/src/main/java/org/openmrs/module/atd/db/hibernate/HibernateATDDAO.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.hibernate.Criteria;
